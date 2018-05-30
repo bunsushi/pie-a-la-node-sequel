03ef17f9d6c1f7a25158f7ac76996d537142c079
bunsushi <tbunsu@gmail.com>
2018-05-30 15:09:40 -0400

    toggle functioning

--- routes/pie_router.js
+++ routes/pie_router.js
@@ -2,29 +2,48 @@ var express = require("express");
 var router = express.Router();
 // var pie = require("../models/pie.js");
 
-module.exports = function(router, Pie) {
+module.exports = function (router, Pie) {
     // Landing Page
-    router.get("/", function(req, res) {
+    router.get("/", function (req, res) {
         Pie.findAll({})
-        .then(function (data) {
-            var hbsObject = {
-                pies: data
-            };
-            res.render("index", hbsObject)
-        });
+            .then(function (data) {
+                var hbsObject = {
+                    pies: data
+                };
+                res.render("index", hbsObject)
+            });
     });
 
-    router.get("/api/pies", function(req, res) {
+    router.get("/api/pies", function (req, res) {
         Pie.findAll({
         }).then(function (result) {
             res.json(result);
         });
     });
 
-    router.post("/api/pies", function(req,res) {
+    router.post("/api/pies", function (req, res) {
         Pie.create(req.body).then(function (result) {
             res.json(result);
         });
+    });
+
+    router.put("/api/pies/:id", function (req, res) {
+        var condition = req.params.id;
+        Pie.update({
+            stock: req.body.stock
+        },
+            {
+                where: {
+                    id: condition
+                }
+            }).then(function (result) {
+                if (result.changedRows == 0) {
+                    return res.status(404).end();
+                }
+                else {
+                    res.status(200).end();
+                }
+            });
     });
 
 };
@@ -66,7 +85,7 @@ module.exports = function(router, Pie) {
 
 // router.delete("/api/pies/:id", function(req, res) {
 //     var condition = "id = " + req.params.id;
-  
+
 //     pie.delete(condition, function(result) {
 //       if (result.affectedRows == 0) {
 //         // If no rows were changed, then the ID must not exist, so 404
